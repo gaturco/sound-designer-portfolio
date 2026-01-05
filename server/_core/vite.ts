09e2d4c4bb9bf0915bc496794b4382f0ce256aca
gaturco <turco.gabriel@live.com>
2026-01-05 15:55:09 -0300

    feat: content type

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -62,6 +62,9 @@ export function serveStatic(app: Express) {
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    const indexPath = path.resolve(distPath, "index.html");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.sendFile(indexPath);
   });
+
 }
